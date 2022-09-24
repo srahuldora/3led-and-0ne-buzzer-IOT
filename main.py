0e5aa8cdf918320a1732b49e02e0d84db536e606
S Rahul Dora <59867500+srahuldora@users.noreply.github.com>
2022-09-24 16:36:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from machine import Pin
-import utime
+from machine import Pin, PWM
+import utime 
+
 
 led_ext1= Pin(15,Pin.OUT)
 led_ext2= Pin(14,Pin.OUT)
@@ -8,9 +9,21 @@ led_ext3= Pin(13,Pin.OUT)
 while True:
   led_ext1.value(1)
   print("EXT LED ON")
-  utime.sleep(5)
+  utime.sleep(3)
+  buzzer = PWM(Pin(15))
+  buzzer.freq(500)
+  buzzer.duty_u16(1000)
+  utime.sleep(3)
+  buzzer.duty_u16(0)
+
   led_ext1.value(0)
   print("EXT LED OFF")
+  buzzer = PWM(Pin(15))
+  buzzer.freq(500)
+  buzzer.duty_u16(1000)
+  utime.sleep(3)
+  buzzer.duty_u16(0)
+
   utime.sleep(1)
   led_ext2.value(1)
   print("EXT LED ON")
